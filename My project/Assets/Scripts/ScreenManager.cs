de9681856f8263bc64ed475cd19edeabb6bcbd7c
Adrianwo76 <124744337+Adrianwo76@users.noreply.github.com>
2025-02-24 20:52:52 -0600

    se

--- My project/Assets/Scripts/ScreenManager.cs
+++ My project/Assets/Scripts/ScreenManager.cs
@@ -1,10 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 
 public class ScreenManager : MonoBehaviour
 {
-    public void LoadScene (string sceneName)
+    public void LoadScene(string sceneName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene (sceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
+
